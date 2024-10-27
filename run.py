5dc27df55942552813663db238e6e1865af4909d
kingizzu <138001977+kingizzu@users.noreply.github.com>
2024-10-27 11:21:54 +0800

    Update run.py

--- run.py
+++ run.py
@@ -129,8 +129,8 @@ if (population[0][1] == 0):
         st.write('Target found')
         st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
         break
-        st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
-        generation+=1
+     st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+     generation+=1
 
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
 
