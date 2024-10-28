3cb25c100c34e07a28fab09df367bdc9061d656f
kingizzu <138001977+kingizzu@users.noreply.github.com>
2024-10-29 00:33:21 +0800

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ POP_SIZE = 500
 GENES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 # User inputs for target string and mutation rate
-TARGET = st.text_input("Enter your name", "Amirul")
+TARGET = st.text_input("Enter your name", "Raja")
 MUT_RATE = st.number_input("Enter your mutation rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
 
 def initialize_pop(TARGET):
